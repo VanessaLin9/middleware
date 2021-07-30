8c930e99fb0ae0903a1a35143b002450a04f7716
VanessaLin9 <vanessa7591@gmail.com>
2021-07-30 13:28:09 +0800

    feat: add onHeader & onFinished

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 // app.js
 const { time } = require('console');
 const express = require('express')
+const onFinished = require('on-finished')
+const onHeaders = require('on-headers')
 const app = express()
 const port = 3000
+let reqTime = ""
+let resTime = ""
+
 
 app.use((req, res, next) => {
-  console.log('有訊息進入');
-  let date = new Date
-  console.log(`${date.toLocaleString()} | ${req.method} from ${req.originalUrl}`);
-  next();
+  console.log('有訊息進來了');
+  reqTime = new Date();
+
+  onHeaders(res, function () {
+    resTime = new Date();
+  })
+
+  onFinished(res, function () {
+    console.log(`${reqTime.toLocaleString()} | ${req.method} from ${req.originalUrl}| total time: ${resTime - reqTime} ms`)
+  })
+
+  next()
 })
 
 app.get('/', (req, res) => {
-  res.send('列出全部 Todo')
+  res.send('列出全部 Todo');
+
 })
 
 app.get('/new', (req, res) => {
-  res.send('新增 Todo 頁面')
+  res.send('新增 Todo 頁面');
 })
 
 app.get('/:id', (req, res) => {
